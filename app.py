fda5eee5f8b6773c9b0540ba54962c5e83058c63
rashmi-pujara <rashmi.pujara1@gmail.com>
2023-02-08 16:22:25 -0500

    cahnges for grammar check

--- app.py
+++ app.py
@@ -17,10 +17,6 @@ model = T5ForConditionalGeneration.from_pretrained(model_name, use_auth_token=au
 model_name_que_check = 'random_forest.joblib'
 rf_que_check = joblib.load(model_name_que_check)
 
-
-model_name_grammar_check = 'random_forest_grammar_check.joblib'
-rf_grammar_check = joblib.load(model_name_grammar_check)
-
 def createEmbedder():
     embedding_method = "gsarti/scibert-nli"
     word_embedding_model = models.Transformer(embedding_method, max_seq_length=512, do_lower_case=True)
@@ -37,16 +33,16 @@ def correct_grammar_suggestion(num_return_sequences=2, model=model):
         qa = question_or_not_check(temp_input)
         if(qa["output"] == 1 and input[-1]!='?'):
             temp_input += "?"
-        cg = correct_grammar_or_not(temp_input)
-        if(cg["output"] == 0):
-            batch = tokenizer([temp_input], truncation=True, padding='max_length',
-                            max_length=64, return_tensors="pt").to(torch_device)
-            translated = model.generate(**batch, max_length=64, num_beams=4,
-                                        num_return_sequences=num_return_sequences, temperature=1.5)
-            tgt_text = tokenizer.batch_decode(translated, skip_special_tokens=True)
-            return {"opration": "success", "grammar_corrected" : 1,"input": input, "output": tgt_text}
-        else:
-            return {"opration": "success", "grammar_corrected" : 0,"input": input, "output": input}
+        batch = tokenizer([temp_input], truncation=True, padding='max_length',
+                        max_length=64, return_tensors="pt").to(torch_device)
+        translated = model.generate(**batch, max_length=64, num_beams=4,
+                                    num_return_sequences=num_return_sequences, temperature=1.5)
+        tgt_text = tokenizer.batch_decode(translated, skip_special_tokens=True)
+        grammar_corrected = 1
+        if temp_input == tgt_text[0] or temp_input == tgt_text[1]:
+            grammar_corrected = 0
+        return {"opration": "success", "grammar_corrected" : grammar_corrected,"input": input, "output": tgt_text}
+        
     except Exception as e:
         return {"opreation": "failed", "error": traceback.format_exc()}
 
@@ -58,18 +54,6 @@ def question_or_not_check(sentence):
             sentence = request.form["sentence"]
         emb = embedder.encode(sentence)
         output = rf_que_check.predict([emb])[0]
-        return {"opration": "success", "input": sentence, "output": str(output)}
-    except Exception as e:
-        return {"opreation": "failed", "error": traceback.format_exc()}
-
-
-@app.route("/api/correct_grammar_or_not", methods=["POST"], defaults={'sentence': None})
-def correct_grammar_or_not(sentence):
-    try:
-        if(sentence == None):
-            sentence = request.form["sentence"]
-        emb = embedder.encode(sentence)
-        output = rf_grammar_check.predict([emb])[0]
         return {"opration": "success", "input": sentence, "output": str(output)}
     except Exception as e:
         return {"opreation": "failed", "error": traceback.format_exc()}
